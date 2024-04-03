776de2ced91f5819ddae8a315e7d0445bb4eac04
blackfireblast <skyblue722@hotmail.com>
2024-04-03 15:02:04 -0600

    Agregar animacion a testimonios

--- src/components/Testimonio.js
+++ src/components/Testimonio.js
@@ -3,7 +3,12 @@ import React from 'react'
 function Testimonio({imagen,ocupacion,descripcion}) {
   return (
     <div className='tarjeta col-12 col-sm-6 col-lg-3'>
-                <img className='ImagenTestimonio' src={`../../assets/${imagen}`} />               
+      
+                <div className='contenedorImagen'>
+                  <img className='ImagenTestimonio' src={`../../assets/${imagen}`} />               
+                  <div className='cuadroImagen'></div>
+                </div>
+
                 <p className='ocupacion'>{ocupacion}</p>
                 <div className='subTarjeta'>
                     <img className='estrellas' src='../../assets/Estrellas.png'/>
